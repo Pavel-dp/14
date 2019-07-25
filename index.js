7e18ffdc6c22014220f11336acb7582c47a4aad0
Pavel-dp <43936378+Pavel-dp@users.noreply.github.com>
2019-07-25 13:07:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
   
+const request = require('request');
+
+request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=3&fbclid=IwAR167NVzmRPLngqPjna9K8IRdBJppfCCVIVOW1NEia5FmYxhJUl8SnJW4xY', { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  console.log(JSON.stringify(body));
+  
+});
